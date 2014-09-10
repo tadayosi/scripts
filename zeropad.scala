177b1c14fd891b2cd28f99546afbe8f1f63de9ea
Tadayoshi Sato <tadayosi@gmail.com>
2014-09-11 00:21:22 +0900

    zeropad.scala: updated

--- zeropad.scala
+++ zeropad.scala
@@ -1,18 +1,17 @@
 #!/usr/bin/env scala
-!#
 
 import java.io._
 
 if (args.length < 3 || args(0).contains("help")) {
   println("""usage:
   > zeropad.scala <target dir> <file extension> <filename length>""")
-  exit
+  sys.exit
 }
 
-new File(args(0)).listFiles filter {
-  file => file.getName.endsWith(args(1))
-} foreach { file =>
+new File(args(0)).listFiles
+  .filter(_.getName.endsWith(args(1)))
+  .foreach { file =>
   val newName = file.getName.reverse.padTo(args(2).toInt + args(1).length, '0').reverse
   println(file.getName + " -> " + newName)
-  file.renameTo(new File(newName))
+  file.renameTo(new File(file.getParent, newName))
 }
